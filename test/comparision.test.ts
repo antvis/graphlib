8c9382a265326e1c96daee26155ac7f5e84dd607
多牧 <xuzhi.mxz@antfin.com>
2022-04-14 17:46:20 +0800

    test: complete tests

--- test/comparision.test.ts
+++ test/comparision.test.ts
@@ -343,14 +343,60 @@ describe('comparision', function () {
       aGraph.setNode('c');
       aGraph.setEdge('a', 'b', 'a');
       aGraph.setEdge('b', 'c', 'b');
+      const bGraph = new Graph();
+      bGraph.setNode('a');
+      bGraph.setNode('b');
+      bGraph.setNode('c');
+      bGraph.setEdge('a', 'b', 'a');
+      bGraph.setEdge('a', 'c', 'a');
+      expect(comparision.isGraphComplement(aGraph, bGraph)).toBe(false);
+    });
+
+    it("returns false when the first graph's nodes is not same with another", function () {
+      const aGraph = new Graph();
+      aGraph.setNode('a');
+      aGraph.setNode('b');
+      aGraph.setNode('c');
+      aGraph.setEdge('a', 'b', 'a');
+      aGraph.setEdge('b', 'c', 'b');
+      aGraph.setEdge('c', 'd', 'c');
+      const bGraph = new Graph();
+      bGraph.setNode('a');
+      bGraph.setNode('b');
+      bGraph.setNode('d');
+      bGraph.setEdge('a', 'b', 'a');
+      bGraph.setEdge('c', 'e', 'c');
+      expect(comparision.isGraphComplement(aGraph, bGraph)).toBe(false);
+    });
+
+    it('returns false when first graph is not a simple graph', function () {
+      const aGraph = new Graph({
+        multigraph: true,
+        directed: false,
+      });
+      aGraph.setNode('a');
+      aGraph.setNode('b');
+      aGraph.setNode('c');
+      aGraph.setEdge('a', 'b', 'a');
+      aGraph.setEdge('b', 'a', 'b');
       aGraph.setEdge('c', 'd', 'c');
       const bGraph = new Graph();
       bGraph.setNode('a');
       bGraph.setNode('b');
       bGraph.setNode('c');
       bGraph.setEdge('a', 'b', 'a');
-      bGraph.setEdge('b', 'd', 'b');
-      bGraph.setEdge('c', 'e', 'c');
+      bGraph.setEdge('b', 'c', 'b');
+      bGraph.setEdge('c', 'd', 'c');
+      expect(comparision.isGraphComplement(aGraph, bGraph)).toBe(false);
+    });
+
+    it("return false when two graph's node counts different", () => {
+      const aGraph = new Graph();
+      const bGraph = new Graph();
+      aGraph.setNode('a');
+      aGraph.setNode('v');
+      bGraph.setNode('b');
+      bGraph.setGraph('w');
       expect(comparision.isGraphComplement(aGraph, bGraph)).toBe(false);
     });
   });
--- test/essence.test.ts
+++ test/essence.test.ts
@@ -1,5 +1,5 @@
 import { Graph } from '../src';
-import { hasSelfLoop, isGraph, isSimpleGraph } from '../src/essence';
+import { hasSelfLoop, isGraph, isNullGraph, isSimpleGraph } from '../src/essence';
 
 describe('Essence', () => {
   it('is graph', () => {
@@ -32,12 +32,31 @@ describe('Essence', () => {
       expect(isSimpleGraph(g)).toBeTruthy();
     });
 
+    it('should return false when graph has self loop', () => {
+      const g = new Graph();
+      g.setEdge('a', 'a');
+      expect(isSimpleGraph(g)).toBeFalsy();
+    });
+
     it('should return false', () => {
       const g = new Graph();
       g.setEdge('a', 'b');
       g.setEdge('b', 'c');
       g.setEdge('c', 'b');
       expect(isSimpleGraph(g)).toBeFalsy();
+    });
+  });
+
+  describe('is null graph', () => {
+    it('should return true', () => {
+      const g = new Graph();
+      expect(isNullGraph(g)).toBeTruthy();
+    });
+
+    it('should return false', () => {
+      const g = new Graph();
+      g.setNode('a');
+      expect(isNullGraph(g)).toBeFalsy();
     });
   });
 });
